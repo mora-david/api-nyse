794eff11454676e936d1c71134ebfb9e82e9ee99
david <david.89.mora@gmail.com>
2021-03-05 16:49:31 -0600

    files

--- nyseApi/urls.py
+++ nyseApi/urls.py
@@ -20,6 +20,8 @@ from django.contrib import admin
 from django.urls import path, include
 from valores.views import EmpresaViewSet, ValoresViewSet
 from rest_framework import routers
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = routers.DefaultRouter()
 
@@ -29,4 +31,4 @@ router.register('valores', ValoresViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
