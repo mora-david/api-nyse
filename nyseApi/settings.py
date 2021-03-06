f5174c2a99d996be54ce6bce03425bdd785a2266
david <david.89.mora@gmail.com>
2021-03-06 08:55:47 -0600

    cors

--- nyseApi/settings.py
+++ nyseApi/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -59,9 +60,8 @@ MIDDLEWARE = [
 
 
 
-ALLOWED_HOSTS=['*']
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_CREDENTIALS = False
 
 ROOT_URLCONF = 'nyseApi.urls'
 
